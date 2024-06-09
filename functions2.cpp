c51fa08bc269ff42dbb00eae9567719dd25c628c
Gagandeepkhalsa <119283324+Gagandeepkhalsa@users.noreply.github.com>
2024-06-09 11:20:11 +0530

    Add files via upload

---
+++ functions2.cpp
@@ -0,0 +1,12 @@
+// Simple code for the function
+#include<iostream>
+using namespace std;
+void printstuff()
+{
+    cout<<"Writing my first function";
+}
+int main(){
+    printstuff()
+    return 0;
+    
+}
